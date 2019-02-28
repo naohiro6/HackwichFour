be622433f33fbd9e3ba86427f27cb77b9cfaf781
CM Student <cmstudent@UHWO-ACM-18.local>
2019-02-28 10:34:24 -1000

    Hackwich 4 is completed.

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+    
+    var myFriends = ["Sara", "Nicole", "Grant"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFriends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")!
+        let text = myFriends[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
